635138f299b9284c9b4fceeef299fd8c021dda78
Godscovenant Patrick Udofe <Covenantudofe@gmail.com>
2026-04-17 16:09:18 +0100

    remove sqlite completely for deployment stability

--- database.js
+++ database.js
@@ -3,43 +3,24 @@ const path = require("path");
 
 const dbPath = path.join(__dirname, "db.json");
 
-// create file if it doesn't exist
 if (!fs.existsSync(dbPath)) {
-  fs.writeFileSync(dbPath, JSON.stringify({ profiles: [] }, null, 2));
+  fs.writeFileSync(dbPath, JSON.stringify({ profiles: [] }));
 }
 
-function readDB() {
-  return JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+function read() {
+  return JSON.parse(fs.readFileSync(dbPath));
 }
 
-function writeDB(data) {
+function write(data) {
   fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
-  getAll: () => readDB().profiles,
-
-  findByName: (name) =>
-    readDB().profiles.find(p => p.name === name),
-
-  findById: (id) =>
-    readDB().profiles.find(p => p.id === id),
-
+  getAll: () => read().profiles,
+  findByName: (name) => read().profiles.find(p => p.name === name),
   insert: (profile) => {
-    const db = readDB();
+    const db = read();
     db.profiles.push(profile);
-    writeDB(db);
-    return profile;
-  },
-
-  delete: (id) => {
-    const db = readDB();
-    const index = db.profiles.findIndex(p => p.id === id);
-
-    if (index === -1) return false;
-
-    db.profiles.splice(index, 1);
-    writeDB(db);
-    return true;
+    write(db);
   }
 };
